58cd973c977dd2f7b637e4ca0e4de0a78ca07c1c
Sasidhar0102 <50265112+Sasidhar0102@users.noreply.github.com>
2021-06-25 21:28:30 +0530

    Update app.py

--- Body Fitness Prediction/Flask/app.py
+++ Body Fitness Prediction/Flask/app.py
@@ -1,10 +1,16 @@
 import numpy as np
 import pandas as pd
 from flask import Flask, render_template, request
-import pickle
+import requests
+
+# NOTE: you must manually set API_KEY below using information retrieved from your IBM Cloud account.
+API_KEY = "kwB4ALb2-exUR2mJnILKbrcPr4NOpWrn3Tm5wZitXsUn"
+token_response = requests.post('https://iam.cloud.ibm.com/identity/token', data={"apikey": API_KEY, "grant_type": 'urn:ibm:params:oauth:grant-type:apikey'})
+mltoken = token_response.json()["access_token"]
+
+header = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + mltoken}
 
 app = Flask(__name__)
-model = pickle.load(open('../Model Building/fitness.pkl','rb'))
 
 @app.route('/')
 def home():
@@ -21,16 +27,19 @@ def predict():
     input_features =  [float(x) for x in request.form.values()]
     features_value = [np.array(input_features)]
 
-    features_name = ['sad','neutral','happy','step_count','calories_burned','hours_of_sleep','weight_kg']
+    payload_scoring = {"input_data": [{"field": [['sad','neutral','happy','step_count','calories_burned','hours_of_sleep','weight_kg']] , "values": [features_value]}]}
 
-    df = pd.DataFrame(features_value, columns=features_name)
-
-    output = model.predict(df)
-
-    if(output[0] == 0):
-        result = "You are not fit"
+    response_scoring = requests.post('https://us-south.ml.cloud.ibm.com/ml/v4/deployments/5a057f36-51ca-4eac-a353-6ea4721d8b6c/predictions?version=2021-06-25', json=payload_scoring, headers={'Authorization': 'Bearer ' + mltoken})
+    print("Scoring response")
+    print(response_scoring.json())
+    predictions=response_scoring.json()
+    print(predictions)
+    pred=predictions['predictions'][0]['values'][0][0]
+    
+    if(pred == 0):
+            result = "You are not fit"
     else:
-        result = "You are fit"
+            result = "You are fit"
 
     return render_template('web.html',prediction_text=result)
 
